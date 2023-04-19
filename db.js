3de72f6eca19de34e74646805a9fd0fdb181ed7e
Devangptl <devangkumar.patel51@gmail.com>
2023-04-19 16:17:15 +0530

    chamge 1

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
-mongoose.set('strictQuery', true);
 require('dotenv').config();
 const mongoURI = process.env.DATABASE
 
 
 
 const connectToMongo = ()=>{
-    mongoose.connect(mongoURI).then(()=>{
+    mongoose.connect(mongoURI ,{
+        useNewUrlParser : true,
+    }).then(()=>{
         console.log("connection successful");
     }).catch((err)=>console.log("no connection"))
       
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 connectToMongo();
 const app = express()
-const port = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
@@ -15,6 +15,6 @@ app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
 })
