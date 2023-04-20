39ef5923ea54917825b1e6f52cfd3ad169270a37
Devangptl <devangkumar.patel51@gmail.com>
2023-04-20 19:19:33 +0530

    chamge 10

--- db.js
+++ db.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const mongoURI = process.env.DATABASE
 
-
-
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI ,{
         useNewUrlParser : true,
